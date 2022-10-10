5c8349e6b26ffb1817eaad4f0794c001f51662a9
OlgaShopieva <113121816+OlgaShopieva@users.noreply.github.com>
2022-10-10 22:58:07 +0300

    Add files via upload

---
+++ BinaryArray_Index/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1,7 -> такого числа в массиве нет.
+Console.Clear();
+
+Console.WriteLine("Введите количество строк двумерного массива");
+int rowCount = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите количество столбцов двумерного массива");
+int columnCount = int.Parse(Console.ReadLine());
+
+int[,] array = FillArray(rowCount, columnCount, 1, 10);
+PrintArray(array);
+
+Console.WriteLine("Введите индекс строки:");
+int indexRow = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите индекс столбца:");
+int indexColumn = int.Parse(Console.ReadLine());
+
+GiveElementByIndex(array);
+
+int[,] FillArray(int rows, int columns, int min, int max)
+{
+    int[,] filledArray = new int[rows, columns];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            filledArray[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+    return filledArray;
+}
+
+void PrintArray(int[,] inputArray)
+{
+    for (int i = 0; i < inputArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inputArray.GetLength(1); j++)
+        {
+            Console.Write(" " + inputArray[i, j]);
+        }
+        Console.WriteLine();
+    }
+}
+
+void GiveElementByIndex(int[,] arr)
+{
+    if (indexRow > rowCount - 1 || indexColumn > columnCount - 1)
+    {
+        Console.WriteLine("Такого элемента в массиве нет");
+    }
+    else
+    {
+        var elem = arr.GetValue(indexRow, indexColumn);
+        Console.WriteLine($"Под введенными индексами находится следующий элемент: {elem}");
+    }
+
+}
