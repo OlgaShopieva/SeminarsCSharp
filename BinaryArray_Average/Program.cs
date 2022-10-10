a77abf3b1b3f92e22eec5bb5279b4497398f145f
OlgaShopieva <113121816+OlgaShopieva@users.noreply.github.com>
2022-10-10 22:59:39 +0300

    Add files via upload

---
+++ BinaryArray_Average/Program.cs
@@ -0,0 +1,63 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+Console.Clear();
+
+Console.WriteLine("Введите количество строк двумерного массива");
+int rowCount = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите количество столбцов двумерного массива");
+int columnCount = int.Parse(Console.ReadLine());
+
+int[,] array = FillArray(rowCount, columnCount, 1, 10);
+PrintArray(array);
+
+Console.WriteLine();
+Console.WriteLine("Среднее арифметическое столбцов равно:" + String.Join("|", FindAverage(array)));
+
+int[,] FillArray(int rows, int columns, int min, int max)
+{
+    int[,] filledArray = new int[rows, columns];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            filledArray[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+    return filledArray;
+}
+
+void PrintArray(int[,] inputArray)
+{
+    for (int i = 0; i < inputArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inputArray.GetLength(1); j++)
+        {
+            Console.Write(" " + inputArray[i, j]);
+        }
+        Console.WriteLine();
+    }
+}
+
+double[] FindAverage(int[,] array)
+{
+double[] LocalArray = new double[array.GetLength(1)];
+
+double sum = 0;
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    sum = 0;
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        sum += array[j, i];
+    }
+    sum /= array.GetLength(1);
+    LocalArray[i] = sum;
+}
+return LocalArray;
+}
