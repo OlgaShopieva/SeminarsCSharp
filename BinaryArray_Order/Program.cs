391d3f807135ecba68fba3ab326eca9467b9644c
OlgaShopieva <113121816+OlgaShopieva@users.noreply.github.com>
2022-10-13 15:05:45 +0300

    Add files via upload

---
+++ BinaryArray_Order/Program.cs
@@ -0,0 +1,76 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+Console.Clear();
+
+Console.WriteLine("Введите количество строк двумерного массива");
+int rowCount = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите количество столбцов двумерного массива");
+int columnCount = int.Parse(Console.ReadLine());
+
+int[,] array = FillArray(rowCount, columnCount, 1, 10);
+
+Console.WriteLine ("Исходный массив: ");
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine("Упорядоченный массив по убыванию: ");
+OrderArray(array);
+PrintArray(array);
+
+
+int[,] FillArray(int rows, int columns, int min, int max)
+{
+    int[,] filledArray = new int[rows, columns];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            filledArray[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+    return filledArray;
+}
+
+void PrintArray(int[,] inputArray)
+{
+    for (int i = 0; i < inputArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inputArray.GetLength(1); j++)
+        {
+            Console.Write(" " + inputArray[i, j]);
+        }
+        Console.WriteLine();
+    }
+}
+
+void OrderArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int y = 0; y < arr.GetLength(1) - 1; y++)
+            {
+                 if (arr[i, y] < arr[i, y + 1])
+                 {
+                     int temp = arr[i, y + 1];
+                     arr[i, y + 1] = arr[i, y];
+                     arr[i,y] = temp; 
+                 }
+                
+            }
+        }
+    }
+
+
+
+
+}
